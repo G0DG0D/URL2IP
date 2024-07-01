7b652e744db11e2b8ca392e9eb72ca32d234b147
GodGod <esponjabobb3@gmail.com>
2024-07-01 14:42:03 +0300

    Update url2ip.py
    se agrego obtener el codigo de estado de la direccion ip opcionalmente usando -s --status_code , y tambien poder obtener la direccion ip de subdominios , seguire trabajando en la tool , esperen mas actualizaciones y mejoras gracias...

--- url2ip.py
+++ url2ip.py
@@ -2,8 +2,9 @@ import socket
 import argparse
 import requests
 from colorama import init, Fore, Style
-banner = """
 
+# Banner inicial
+banner = """
 GGGGGGG     OOOOOO   DDDDDD    GGGGGGG   OOOOOO   DDDDDD
 G          O      O  D     D   G        O      O  D     D
 G   GGGG   O      O  D      D  G   GGGG O      O  D      D
@@ -12,27 +13,48 @@ GGGGGGG     OOOOOO   DDDDDD    GGGGGGG   OOOOOO   DDDDDD
 """
 print(banner)
 
+# Inicialización de colorama
 init()
 
-def obtener_ip_y_verificar_estado(url):
+def obtener_ip_y_verificar_estado(url, mostrar_status_code):
     try:
-        ip_address = socket.gethostbyname(url)
-                
-        response = requests.get(f'http://{ip_address}')
+        addr_info = socket.getaddrinfo(url, None , socket.AF_INET)
+        ip_addresses = [ai[4][0] for ai in addr_info]
         
-        if response.status_code == 200:
-            estado = f'[{Fore.GREEN}{response.status_code}{Style.RESET_ALL}]'
-        elif response.status_code == 404:
-            estado = f'[{Fore.RED}{response.status_code}{Style.RESET_ALL}]'
-        return f'La dirección IP de {url} es: {Fore.GREEN}{ip_address}{Style.RESET_ALL}{estado}'
+        respuesta = None 
+        
+        for ip_address in ip_addresses:
+            try:
+                response = requests.get(f'http://{ip_address}', timeout=5)
+                
+                if response.status_code == 200:
+                    estado = f'[{Fore.GREEN}{response.status_code}{Style.RESET_ALL}]'
+                elif response.status_code == 404:
+                    estado = f'[{Fore.RED}{response.status_code}{Style.RESET_ALL}]'
+                else:
+                    estado = f'[{Fore.YELLOW}{response.status_code}{Style.RESET_ALL}]'
+                    
+                respuesta = f'La direccion IP de {url} es: {Fore.GREEN}{ip_address}{Style.RESET_ALL}'
+                
+                if mostrar_status_code:
+                    respuesta += f' {estado}'
+                    
+                break
+            except requests.RequestException:
+                continue
+        if respuesta is None:
+            respuesta = f'Error: No se pudo realizar la solicitud HTTP'
+            
     except socket.gaierror:
-        return f"Error: No se pudo resolver la dirección para {url}"
-    except requests.RequestException as e:
-        return f"Error: No se pudo realizar la solicitud HTTP {url}"
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Obtener la direccion IP de una URL. ')
-    parser.add_argument('--url', '-u', type=str, required=True , help='ingrese solo el dominio ejemplo: example.com')
-    arg = parser.parse_args()
-    resultado = obtener_ip_y_verificar_estado(arg.url)
-    print(resultado)
+        respuesta = f'Error: No se pudo resolver la direccion para {url}'
+        
+    return respuesta
 
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Obtener la direccion Ip y Opcionalmente el codigo de estado HTTP de una url')
+    parser.add_argument('--url','-u', type=str, required=True, help='Ingrese solo el dominio, ejemplo: example.com')
+    parser.add_argument('--status_code', '-s', action='store_true', help='Mostrar el código de estado HTTP.')
+    args = parser.parse_args()
+    
+    resultado = obtener_ip_y_verificar_estado(args.url, args.status_code)
+    print(resultado)
