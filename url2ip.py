6001b549d19cc958a37d8ecd9c4b999e3b412ff8
GodGod <esponjabobb3@gmail.com>
2024-07-04 13:39:42 +0300

    Update url2ip.py
    Se agrego trabajar con multiples urls al a vez . solo ingresalas sin el 'htts:// ',  solamente asi: example.com , sub.example.com, vamos mejorando poco a poco...
    Happy Hacking : )

--- url2ip.py
+++ url2ip.py
@@ -1,7 +1,9 @@
+import os
 import socket 
 import argparse
 import requests
-from colorama import init, Fore, Style
+from requests.exceptions import SSLError, ConnectionError
+from colorama import init, Fore, Back , Style
 
 # Banner inicial
 banner = """
@@ -15,7 +17,7 @@ print(banner)
 
 # Inicialización de colorama
 init()
-
+    
 def obtener_ip_y_verificar_estado(url, mostrar_status_code):
     try:
         addr_info = socket.getaddrinfo(url, None , socket.AF_INET)
@@ -31,30 +33,59 @@ def obtener_ip_y_verificar_estado(url, mostrar_status_code):
                     estado = f'[{Fore.GREEN}{response.status_code}{Style.RESET_ALL}]'
                 elif response.status_code == 404:
                     estado = f'[{Fore.RED}{response.status_code}{Style.RESET_ALL}]'
+                elif response.status_code == 403:
+                    estado = f'[{Fore.YELLOW}{response.status_code}{Style.RESET_ALL}]' 
+                elif response.status_code == 401:
+                    estado = f'[{Fore.BLUE}{response.status_code}{Style.RESET_ALL}]'
                 else:
                     estado = f'[{Fore.YELLOW}{response.status_code}{Style.RESET_ALL}]'
                     
-                respuesta = f'La direccion IP de {url} es: {Fore.GREEN}{ip_address}{Style.RESET_ALL}'
+                respuesta = f'{Fore.MAGENTA}La direccion IP de {url} es: {Fore.CYAN}{ip_address}{Style.RESET_ALL}'
                 
                 if mostrar_status_code:
                     respuesta += f' {estado}'
                     
                 break
+            except SSLError:
+                respuesta = f'Error: Problema de certificado SSL al acceder a {url}'
+            except ConnectionError:
+                respuesta = f'[!]Error: No se puede conectar a {url}'
             except requests.RequestException:
                 continue
         if respuesta is None:
-            respuesta = f'Error: No se pudo realizar la solicitud HTTP'
-            
+            respuesta = f'Error: No se pudo realizar la solicitud HTTP a {url}'
     except socket.gaierror:
         respuesta = f'Error: No se pudo resolver la direccion para {url}'
-        
     return respuesta
+def leer_archivos_de_archivo(ruta_archivo):
+    try:
+        with open(ruta_archivo, 'r') as archivo:
+            urls = archivo.read().splitlines()
+        return urls 
+    except FileNotFoundError:
+        print(f'El archivo {ruta_archivo} no se encontro')
+        return []
+    except Exception as e:
+        print(f'Se produjo un error al leer el {ruta_archivo}: {e}')
+        return []
 
 if __name__ == '__main__':
+    
     parser = argparse.ArgumentParser(description='Obtener la direccion Ip y Opcionalmente el codigo de estado HTTP de una url')
-    parser.add_argument('--url','-u', type=str, required=True, help='Ingrese solo el dominio, ejemplo: example.com')
+    parser.add_argument('--url','-u', type=str, help='Ingrese solo el dominio, ejemplo: example.com')
     parser.add_argument('--status_code', '-s', action='store_true', help='Mostrar el código de estado HTTP.')
+    parser.add_argument('--list','-l', type=str, help='Pasa una lista de urls.txt')
     args = parser.parse_args()
     
-    resultado = obtener_ip_y_verificar_estado(args.url, args.status_code)
-    print(resultado)
+    if args.url:
+        resultado = obtener_ip_y_verificar_estado(args.url, args.status_code)
+        print(resultado)
+    elif args.list:
+        urls = leer_archivos_de_archivo(args.list)
+        for url in urls:
+            url = url.strip()
+            if url:
+                resultado = obtener_ip_y_verificar_estado(url, args.status_code)
+                print(resultado)
+    else:
+        print('debe proporcionar una url con --url o un archivo con --list')
